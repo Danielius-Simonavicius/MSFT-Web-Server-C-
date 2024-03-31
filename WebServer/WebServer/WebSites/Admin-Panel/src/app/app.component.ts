135481508f2a11475921f04c4ef31c313eaddba0
Danielius-Simonavicius <Daniel@theguy.xyz>
2024-03-31 23:54:40 +0100

    g

--- WebServer/WebServer/WebSites/Admin-Panel/src/app/app.component.ts
+++ WebServer/WebServer/WebSites/Admin-Panel/src/app/app.component.ts
@@ -1,44 +1,71 @@
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Guid } from 'guid-typescript';
 import { Meta } from '@angular/platform-browser';
+import { NgForm } from '@angular/forms';
+interface FormData {
+  name: string;
+  description: string;
+  guid: string
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'Admin-Panel';
-  name: string = "";
-  file: any;
+  @ViewChild('myForm') Form: NgForm;
+  title = "Admin-Panel";
+  formData: FormData = {
+    name: '',
+    description: '',
+    guid: ''
+  };
+  fileToUpload: File | undefined;
 
-  constructor() {
+  constructor() { }
+
+  handleFileInput(event: any) {
+    this.fileToUpload = event.target.files[0];
   }
 
-  getName(name: string) {
-    this.name = name;
-  }
-  getFolder(event: any) {
-    this.file = event.target.files[0];
-    console.log('file', this.file)
-  }
+  submitForm() {
+    const formData = new FormData(this.Form.form);
+    formData.append('name', this.formData.name);
+    formData.append('description', this.formData.description);
+    this.formData.name = Guid.create().toString();
+    formData.append('guid', this.formData.guid);
 
-  submitData() {
-    let formData = new FormData();
-    formData.set("name", this.name);
-    formData.set("file", this.file);
-    fetch('http://localhost:9090/uploadWebsite', { 
-      method: 'POST',
-      body: formData
-    }).then(res => {
-      return res.json()
-    })
-    .then(data => console.log(data))
-    .catch(errpr => console.log('ERROR'))
-    // this.http.post('http://localhost:9090/uploadWebsite', formData).subscribe(
-    //   (response) => {
+    // Check if fileToUpload is defined before appending it
+    if (this.fileToUpload) {
+      formData.append('folder', this.fileToUpload);
+    }
+    this.formDa
 
+    // this.http.post<any>('http://localhost:9090/uploadWebsite', formData).subscribe(
+    //   response => {
+    //     console.log('Upload successful:', response);
+    //   },
+    //   error => {
+    //     console.error('Upload failed:', error);
     //   }
-    // )
+    // );
+
+    // submitData(form: HTMLFormElement):void {
+
+    //   form.submit()
+    //   // formData.set("name", this.name);
+    //   // formData.set("file", this.file);
+    //   // fetch('http://localhost:8085/', { 
+    //   //   method: 'POST',
+    //   //   body: formData
+    //   // }).then(res => {
+    //   //   return res.json()
+    //   // })
+    //   // .then(data => console.log(data))
+    //   // .catch(errpr => console.log('ERROR'))
+    // }
   }
 }

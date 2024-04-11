e5e3b3a14020449057f2962e4cd298548b13cd45
Danielius-Simonavicius <Daniel@theguy.xyz>
2024-04-11 14:19:02 +0100

    fixing up StartListeningForData()

--- WebServer/WebServer/WorkerService.cs
+++ WebServer/WebServer/WorkerService.cs
@@ -31,7 +31,7 @@ public class WorkerService : BackgroundService
 
     private readonly IHttpRequestParser _parser;
 
-    public WorkerService(ILogger<WorkerService> logger, 
+    public WorkerService(ILogger<WorkerService> logger,
         IHttpRequestParser parser, IOptions<ServerConfigModel> config)
     {
         _config = config.Value;
@@ -47,22 +47,22 @@ public class WorkerService : BackgroundService
 
         foreach (var website in websites)
         {
-            _thread = new Thread(() => ConnectionThreadMethod(website,stoppingToken));
+            _thread = new Thread(() => ConnectionThreadMethod(website, stoppingToken));
             _thread.Start();
         }
     }
-    
+
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         await Task.Yield();
         StartServer(stoppingToken);
         while (!stoppingToken.IsCancellationRequested)
         {
-            
             if (_requestsQueue.TryDequeue(out var requestModel) && requestModel.Client != null)
             {
                 var handler = requestModel.Client;
-                var hostParts = requestModel.Host.Split(":"); //hostParts = localhost:8085 (trying to find port e.g. "8085")
+                var hostParts =
+                    requestModel.Host.Split(":"); //hostParts = localhost:8085 (trying to find port e.g. "8085")
                 int port = IntegerType.FromString(hostParts[1]);
                 await handler.SendToAsync(GetResponse(requestModel,
                     _config.Websites.First((x) => x.WebsitePort == port)), handler.RemoteEndPoint!, stoppingToken);
@@ -77,7 +77,7 @@ public class WorkerService : BackgroundService
     {
         try
         {
-            IPEndPoint endPoint = new IPEndPoint(IPAddress.Loopback, website.WebsitePort);
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, website.WebsitePort);
             //var httpServer = new Socket(SocketType.Stream, ProtocolType.Tcp);
             //httpServer.Bind(endPoint);
             //httpServer.Listen(100);
@@ -90,18 +90,19 @@ public class WorkerService : BackgroundService
             }
             //_ = StartListeningForData(httpServer, token);
             //var handler = new HttpClientHandler();
-
         }
-        catch(Exception ex)
+        catch (Exception ex)
         {
             Console.WriteLine($"{website.Path} Server could not start: {ex.Message}");
         }
     }
-    
-    
+
+
     private async Task ProcessClientAsync(TcpClient client, CancellationToken token)
-    {   
-        X509Certificate2 serverCertificate = new X509Certificate2("D:\\MicrosoftProj\\MSFT-Web-Server\\WebServer\\WebServer\\Files\\MSFTServer.pfx","microsoftProject");
+    {
+        X509Certificate2 serverCertificate = new X509Certificate2(
+            "/Users/danieljr/Desktop/Projects/MSFT-Web-Server-C-/WebServer/WebServer/certificates/MSFTServer.pfx",
+            "microsoftProject");
 
         using (SslStream sslStream = new SslStream(client.GetStream(), false))
         {
@@ -114,7 +115,7 @@ public class WorkerService : BackgroundService
             catch (Exception ex)
             {
                 _logger.LogError($"SSL/TLS handshake failed: {ex.Message}");
-                
+
                 // Optionally, you can handle specific exceptions and provide more detailed error messages
                 if (ex is AuthenticationException authEx)
                 {
@@ -137,64 +138,64 @@ public class WorkerService : BackgroundService
             }
         }
     }
-                
-                
-                // Proceed with secure communication
-            
-            /*catch (AuthenticationException e)
-            {
-                Console.WriteLine($"Server authentication failed: {e.Message}");
-                // Handle authentication failure
-            }*/
-        
-    
+
+
+    // Proceed with secure communication
+
+    /*catch (AuthenticationException e)
+    {
+        Console.WriteLine($"Server authentication failed: {e.Message}");
+        // Handle authentication failure
+    }*/
+
+
     private async Task StartListeningForData(SslStream sslStream, CancellationToken token)
     {
-            //X509Certificate2 serverCertificate = new X509Certificate2("D:\\MicrosoftProj\\MSFT-Web-Server\\WebServer\\WebServer\\Files\\MSFTServer.pfx","microsoftProject");
-            //X509Certificate2 clientCertificate = new X509Certificate2("D:\\MicrosoftProj\\MSFT-Web-Server\\WebServer\\WebServer\\Files\\clientca+key.pfx","microsoftProject");
-            //handler.ClientCertificates.Add(clientCertificate );
-            //var handler = await httpServer.AcceptAsync(token);
-            //X509Certificate2 cert = GetCertificateFromStore("CN=CERT_SIGN_TEST_CERT");
+        //X509Certificate2 serverCertificate = new X509Certificate2("D:\\MicrosoftProj\\MSFT-Web-Server\\WebServer\\WebServer\\Files\\MSFTServer.pfx","microsoftProject");
+        //X509Certificate2 clientCertificate = new X509Certificate2("D:\\MicrosoftProj\\MSFT-Web-Server\\WebServer\\WebServer\\Files\\clientca+key.pfx","microsoftProject");
+        //handler.ClientCertificates.Add(clientCertificate );
+        //var handler = await httpServer.AcceptAsync(token);
+        //X509Certificate2 cert = GetCertificateFromStore("CN=CERT_SIGN_TEST_CERT");
 
-            //SslStream sslStream = new SslStream(new NetworkStream(handler), false);
-            try
+        //SslStream sslStream = new SslStream(new NetworkStream(handler), false);
+        try
+        {
+            while (!token.IsCancellationRequested)
             {
-                byte[] buffer = new byte[1024];
-                int bytesRead;
                 var data = "";
+                byte[] bytes = new byte[1024];
+                //int bytesRead;
 
                 while (!token.IsCancellationRequested)
                 {
-                    bytesRead = await sslStream.ReadAsync(buffer, 0, buffer.Length, token);
-                    if (bytesRead == 0)
-                    {
-                        // No more data to read, exit the loop
-                        break;
-                    }
+                    var received = await sslStream.ReadAsync(bytes, 0, bytes.Length);
+                    var partialData = Encoding.ASCII.GetString(bytes, 0, received);
 
-                    string partialData = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    //string partialData = Encoding.UTF8.GetString(bytes, 0, bytesRead);
                     data += partialData;
 
                     if (data.Contains("\r\n"))
                     {
                         break;
                     }
-                    // End of HTTP request detected, process the request
-                    LogRequestData(data);
-                    var request = _parser.ParseHttpRequest(data);
-                    // Since you're not sending responses, you don't need to set request.Client
-                    _requestsQueue.Enqueue(request);
-
-                    // Reset data for next request
-                    data = string.Empty;
                 }
+
+                // End of HTTP request detected, process the request
+                LogRequestData(data);
+                var request = _parser.ParseHttpRequest(data);
+                // Since you're not sending responses, you don't need to set request.Client
+                _requestsQueue.Enqueue(request);
+
+                // Reset data for next request
+                data = string.Empty;
             }
-            catch (Exception ex)
-            {
-                _logger.LogError($"Error in secure communication: {ex.Message}");
-            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"Error in secure communication: {ex.Message}");
+        }
     }
-    
+
     private byte[] GetResponse(HttpRequestModel requestModel, WebsiteConfigModel website)
     {
         string statusCode = "200 OK";
@@ -212,12 +213,12 @@ public class WorkerService : BackgroundService
         {
             fileName = fileName.Substring(1);
         }
-        
-        
+
+
         var rootFolder = _config.RootFolder;
 
         var requestedFile = Path.Combine(rootFolder, webSite, fileName);
-        
+
         if (methodType.Equals("GET")) // Do we sort methods in here like this? one after the other?
         {
             // put logic in here
@@ -227,55 +228,53 @@ public class WorkerService : BackgroundService
         {
             //put logic in here
         }
-        
-        
+
+
         // File doesn't exist, return 404 Not Found
         if (!File.Exists(requestedFile))
         {
             Console.WriteLine($"File not found: {requestedFile}");
-            return NotFound404(website,statusCode);
+            return NotFound404(website, statusCode);
         }
-        
-        
+
+
         var file = File.ReadAllBytes(requestedFile);
-        
-        
+
+
         //TODO: is there better way to detect??
         string contentType = FindContentType(requestedFile);
-        
+
         String resHeader =
             $"HTTP/1.1 {statusCode}\r\n" +
             "Server: Microsoft_web_server\r\n" +
             $"Content-Type: {contentType}; charset=UTF-8\r\n" +
             $"Access-Control-Allow-Origin: {website.AllowedHosts}\r\n\r\n";
-        
-       var resData = Encoding.ASCII.GetBytes(resHeader).Concat(file);
-        return resData.ToArray();
 
+        var resData = Encoding.ASCII.GetBytes(resHeader).Concat(file);
+        return resData.ToArray();
     }
+
     public static string FindContentType(string requestedFile) =>
         requestedFile.EndsWith(".js") ? "text/javascript" :
         requestedFile.EndsWith(".css") ? "text/css" :
         "text/html";
-    
 
 
-
-    public byte[] NotFound404(WebsiteConfigModel website,string statusCode)
+    public byte[] NotFound404(WebsiteConfigModel website, string statusCode)
     {
         statusCode = "404 Not found";
         String responseHeader =
             $"HTTP/1.1 {statusCode}\r\n" +
             "Server: Microsoft_web_server\r\n" +
             $"Access-Control-Allow-Origin: {website.AllowedHosts}\r\n\r\n";
-        
+
         var responseData = Encoding.ASCII.GetBytes(responseHeader);
         return responseData.ToArray();
     }
-   
+
 
     private void LogRequestData(string requestData)
-    { 
+    {
         _logger.LogInformation($"\n{requestData}");
     }
 }
